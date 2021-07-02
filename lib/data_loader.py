168bfa760293b2c6b181ed0ba6988a5e04b8e3fa
run-byte-run <3127978+run-byte-run@users.noreply.github.com>
2021-07-02 14:48:47 +0300

    Implement data_loader class.

--- lib/data_loader.py
+++ lib/data_loader.py
@@ -1,4 +1,16 @@
+from os.path import dirname, exists, join
+
+
 class DataLoader:
     @staticmethod
     def load_from_file(filename):
-        pass
+        if not exists(filename):
+            filename = join(dirname(__file__), filename)
+
+        try:
+            with open(filename, 'r') as f:
+                data = [line.strip('\r\n ') for line in f]
+        except OSError as e:
+            raise ValueError(f'Can\'t open/read file: {filename}.')
+
+        return data
